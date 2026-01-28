d1293ce03b970e2dbf0e9ad95adf8c63ffeb0e89
Dragonus <davidarielconcon@gmail.com>
2026-01-28 22:38:39 +0800

    Working Open Inventory

--- Assets/Scripts/Scripts_Player/PlayerInventory.cs
+++ Assets/Scripts/Scripts_Player/PlayerInventory.cs
@@ -9,13 +9,12 @@ public class PlayerInventory : MonoBehaviour
 {
     public GameObject InventoryPanel;
     public GameObject SlotPrefab;
-    public GameObject PlayerInventoryUI;
     public GameObject InventoryStatusUI;
 
     public int CurrentWeight;
     public int MaxWeight;
 
-    public void InventoryOnOpenInventory(InputAction.CallbackContext context) => PlayerInventoryUI.SetActive(!PlayerInventoryUI.activeSelf);
+    public void InventoryOnOpenInventory(InputAction.CallbackContext context) => GameManager.Instance.UIManager.OpenUI(UIPanelType.Inventory);
 
     public void AddToInventory(GarbageObject garbageObject)
     {
@@ -33,7 +32,6 @@ public class PlayerInventory : MonoBehaviour
         slot.GarbageDescription.text = garbageObject.ResourceData.ObjectDescription;
     }
 
-
     public void ShowInventoryPanel() => InventoryPanel.SetActive(!InventoryPanel.activeSelf);
 
     public async void UITimerCall() => await InventoryStatusUITimer();
@@ -44,22 +42,4 @@ public class PlayerInventory : MonoBehaviour
         await Task.Delay(2 * 1000);
         InventoryStatusUI.SetActive(false);
     }
-
-    //public bool AddItem(GameObject itemPrefab)
-    //{
-    //    foreach(Transform SlotTransform in InventoryPanel.transform)
-    //    {
-    //        Slot slot = SlotTransform.GetComponent<Slot>();
-
-    //        if(slot != null && slot.currentItem == null)
-    //        {
-    //            GameObject newItem = Instantiate(itemPrefab, SlotTransform);
-    //            newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-    //            slot.currentItem = newItem;
-    //            return true;
-    //        }
-    //    }
-
-    //    return false;
-    //}
 }
