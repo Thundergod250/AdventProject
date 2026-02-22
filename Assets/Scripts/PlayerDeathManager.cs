04cb78983e2ee7c6533903a75bcefdf7ca681bdf
Dragonus <davidarielconcon@gmail.com>
2026-02-22 15:00:25 +0800

    Added Ocean Height Death

--- Assets/Scripts/PlayerDeathManager.cs
+++ Assets/Scripts/PlayerDeathManager.cs
@@ -3,11 +3,28 @@ using System.Collections;
 
 public class PlayerDeathManager : MonoBehaviour
 {
+    [Header("References")]
     public GameObject Player;
     public GameObject SpawnPoint;
-    
+
+    [Header("Death Settings")]
+    [SerializeField] private float lavaHeightThreshold = -3f; //OCEAN HEIGHT
+    [SerializeField] private float respawnDelay = 3f;
+
+    private bool isRespawning = false;
+
     public void CallDeathAndRespawnRoutine() => StartCoroutine(DeathAndRespawnRoutine());
-    
+
+    private void Update()
+    {
+        // Continuously check if player has fallen below threshold
+        if (!isRespawning && Player != null && Player.transform.position.y < lavaHeightThreshold)
+        {
+            Debug.Log("Player fell below lava threshold.");
+            StartCoroutine(DeathAndRespawnRoutine());
+        }
+    }
+
     private void HandlePlayerDeath()
     {
         Player.gameObject.SetActive(false);
@@ -15,9 +32,9 @@ public class PlayerDeathManager : MonoBehaviour
 
         GameManager.Instance.MiningManager.StopMining();
 
-        Debug.Log("Player has been disabled after trigger collision.");
+        Debug.Log("Player has been disabled after trigger collision or lava fall.");
     }
-    
+
     private void HandlePlayerRespawn()
     {
         if (SpawnPoint != null)
@@ -25,18 +42,21 @@ public class PlayerDeathManager : MonoBehaviour
             Player.transform.position = SpawnPoint.transform.position;
             Player.transform.rotation = SpawnPoint.transform.rotation;
         }
-        
+
         Player.gameObject.SetActive(true);
         Player.GetComponent<PlayerManipulator>()._EnableAllMovement();
         GameManager.Instance.GoldManager.ReduceGoldPercentage(0.5f);
+
         Debug.Log("Player has respawned and all movement re-enabled.");
     }
 
     private IEnumerator DeathAndRespawnRoutine()
     {
+        isRespawning = true;
         HandlePlayerDeath();
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(respawnDelay);
         Debug.Log("HandlePlayerRespawn");
         HandlePlayerRespawn();
+        isRespawning = false;
     }
 }
