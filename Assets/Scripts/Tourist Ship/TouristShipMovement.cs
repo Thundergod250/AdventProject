ac86daa0f170e73f7912b50a01390e5b6bf6225a
Ryan Vasquez <ryanlorenzo.vasquez@benilde.edu.ph>
2026-04-29 10:58:24 +0800

    NPC Base Movement - From Boat to Statue - pt1
    - working on Spawning

--- Assets/Scripts/Tourist Ship/TouristShipMovement.cs
+++ Assets/Scripts/Tourist Ship/TouristShipMovement.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TouristShipMovement : MonoBehaviour
 {
     [SerializeField] private Transform boatPointStart; // assign in Inspector
     [SerializeField] private Transform boatPointEnd;   // assign in Inspector
+    [SerializeField] private Transform bridge;   // assign in Inspector
+    [SerializeField] private Vector3 bridgeRestPosition = new Vector3(0.757f, 0.145f, 0f);
+
+    public UnityEvent BridgeCall;
+
     [SerializeField] private float travelTime = 5f;    // time to move from start to end
 
     private float elapsedTime = 0f;
@@ -33,6 +39,12 @@ public class TouristShipMovement : MonoBehaviour
                 isMoving = false;
             }
         }
+        else
+        {
+            bridge.transform.localPosition = Vector3.Lerp(bridge.position, bridgeRestPosition, 1f);
+            BridgeCall.Invoke();
+        }
+
     }
 
     // Call this method to start the ship’s journey
